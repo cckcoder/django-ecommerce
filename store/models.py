f501a938dc76bd640da8c0f51cdeb2a1d306d455
codewizz <c.chaiwat.ch@gmail.com>
2021-12-11 15:29:01 +0000

    add: requirement file; update: model;

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -26,3 +27,14 @@ class Product(models.Model):
     image = models.ImageField(upload_to="images/")
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
