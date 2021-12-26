c9a501d1948c6651c6f1e079cbc9d70ef932064d
codewizz <c.chaiwat.ch@gmail.com>
2021-12-26 17:03:09 +0000

    add: stripe lib

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("store.urls", namespace="store")),
     path("basket/", include("basket.urls", namespace="basket")),
     path("account/", include("account.urls", namespace="account")),
+    path("payment/", include("payment.urls", namespace="payment")),
+    path("", include("store.urls", namespace="store")),
 ]
 
 
